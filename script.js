a6d8b9e9ee18dbbda00984475fc86425f69699dd
Kezya <kjpilao22@src.pshs.edu.ph>
2025-01-24 07:47:16 +0800

    Update script.js

--- script.js
+++ script.js
@@ -1,8 +1,8 @@
 const PI = 3.14;
 var radius = 6.378e6;
-var circumference = 2*PI*radius;
-var surfaceArea = 4*PI*(radius**2);
-var volume = (4/3)*PI*(radius**3);
+var circumference = 2 * PI * radius;
+var surfaceArea = 4 * PI  *(radius**2);
+var volume = (4/3) * PI * (radius**3);
 
 document.write('The radius of the sphere is ', radius, ".");
 document.write('<br><br>');
